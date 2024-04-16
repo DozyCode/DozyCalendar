20dd9153d4bc786b3f9de0db53dce398b3b5684c
Carter Foughty <cfoughty7@gmail.com>
2024-04-16 16:13:03 -0500

    Fix scroll view finding logic

--- Sources/DozyCalendar/View Modifiers/UIScrollViewProvider.swift
+++ Sources/DozyCalendar/View Modifiers/UIScrollViewProvider.swift
@@ -126,7 +126,7 @@ private class ScrollViewTracingView<ViewType: UIView>: UIView {
         // The `ScrollView` can consistently be found one place further along in the view stack,
         // so we iterate one position.
         guard let currentViewIndex = parentView.subviews.firstIndex(where: { $0.accessibilityIdentifier == viewID }),
-              let desiredViewIndex = parentView.subviews.count >= currentViewIndex + 1 ? currentViewIndex + 1 : nil,
+              let desiredViewIndex = parentView.subviews.count > currentViewIndex + 1 ? currentViewIndex + 1 : nil,
               let scrollView = parentView.subviews[desiredViewIndex].subviews.first as? UIScrollView else {
             assertionFailure("ScrollViewTracingView.findScrollView did not capture the UIScrollView.")
             return
